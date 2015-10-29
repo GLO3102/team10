e53254a7e78d4b3b847005a16ae6d5cfd52e7390
Dominic <dominic.fillion.1@gmail.com>
2015-10-29 15:41:30 -0400

    Changed order of imports in index.html

--- src/app/views/movies-view.js
+++ src/app/views/movies-view.js
@@ -4,7 +4,7 @@ var app = app || {};
 
     app.MoviesView = Backbone.View.extend({
 
-        model: app.movie,
+        model: new app.Movie({id:"265727087"}),
 
         template: _.template($('#movies-template').html()),
 
@@ -13,7 +13,12 @@ var app = app || {};
         },
 
         render: function () {
-            console.log("test");
+
+            var that = this;
+
+            this.model.fetch().success(function() {
+                console.log(that.model)
+            });
             //this.$el.html(this.template());
         }
     });
