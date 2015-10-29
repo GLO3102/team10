a86a3f465e927852de182023af0aa900185d8fa0
Olivier Falardeau <olivier.falardeau@gogap.co>
2015-10-29 13:28:47 -0400

    rendering views on route changed

--- src/app/router.js
+++ src/app/router.js
@@ -4,11 +4,38 @@ var app = app || {};
 
     var Router = Backbone.Router.extend({
         routes: {
-            // todo
+            "": "index",
+
+            "/movies": "movies",
+            "/movies/960891136": "movies",
+
+            "/actors": "actors",
+            "/actors/211294246": "actors",
+
+            "/tvshows": "tvshows",
+            "/tvshows/599183923": "tvshows",
+
+            "/watchlists": "watchlists"
         }
     });
 
     app.Router = new Router();
+
+    app.Router.on("route:movies", function() {
+        app.MoviesView.render();
+    });
+
+    app.Router.on("route:actors", function() {
+        app.ActorsView.render();
+    });
+
+    app.Router.on("route:tvshows", function() {
+        app.TvShowsView.render();
+    });
+
+    app.Router.on("route:watchlists", function() {
+        app.WatchlistsView.render();
+    });
 
     Backbone.history.start();
 })();
