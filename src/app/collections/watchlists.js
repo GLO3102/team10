bba57c869195cbf1ab813059478dfb9b39df05ca
Simon <sifrenette@gmail.com>
2015-10-29 17:23:44 -0400

    watchlists

--- src/app/collections/watchlists.js
+++ src/app/collections/watchlists.js
@@ -1,3 +1,9 @@
-/**
- * Created by olfal1 on 2015-10-28.
- */
+var app = app || {};
+
+(function() {
+
+    app.Watchlists = Backbone.Collection.extend({
+        model: app.Watchlist
+    });
+
+})();
