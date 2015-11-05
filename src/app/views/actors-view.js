6d314b2303a9b88056f115be168784ef805a5338
thastreet <mat_1794@hotmail.com>
2015-11-05 16:38:00 -0500

    Fix de responsive.

--- src/app/views/actors-view.js
+++ src/app/views/actors-view.js
@@ -22,8 +22,6 @@ var app = app || {};
             self.model = new app.Actor({id: id});
 
             self.model.fetch().success(function() {
-                console.log(self.model.attributes);
-
                 self.setImageURL(self.model);
 
                 self.$el.html(self.template({actor: self.model.toJSON(), movies: {}}));
